239fef9e5189eb8c7e838277e2b9a08e9154d372
saipulanuar <77055141+saipulanuar@users.noreply.github.com>
2022-01-14 10:00:49 +0700

    p

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -2,11 +2,11 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
 if (!args[0]) throw 'Uhm..url nya mana?'
 m.reply(wait)
-let res = await fetch(`https://rset-apikey.herokuapp.com/api/tiktod/?Apikey=${lolkey}&url=${args[0]}`)
+let res = await fetch(`https://apikey-bear2.herokuapp.com/api/tiktod/?url=${args[0]}&apikey=${bearkey}`)
 if (!res.ok) throw await res.text()
 let json = await res.json()
 if (!json.status) throw json
-let { description, author, statistic, link } = json.result
+let { description, author, statistic, link } = json.nowm
 await conn.sendFile(m.chat, link, 'tt.mp4', `
 ▶ ${statistic.playCount} Views
 ❤ ${statistic.diggCount} Likes
